7395079a9755b5fbecda2c9171cc0b5821232561
César Figueroa <cesar.figueroa@agilesolutions.com>
2017-11-11 12:44:21 -0600

    init proyect

---
+++ src/UI/Application.java
@@ -0,0 +1,72 @@
+package UI;
+
+import com.mongodb.*;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Projections;
+import com.mongodb.client.model.Filters;
+import static com.mongodb.client.model.Filters.*;
+import static com.mongodb.client.model.Projections.*;
+import com.mongodb.client.model.Sorts;
+import java.util.Arrays;
+import org.bson.Document;
+
+public class Application extends javax.swing.JFrame {
+
+    Block<Document> printBlock = new Block<Document>() {
+        @Override
+        public void apply(final Document document) {
+            System.out.println(document.toJson());
+        }
+    };
+
+    public Application() {
+        MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://cfigue:Sheldoncooper@ds259305.mlab.com:59305/fisheman"));
+        MongoDatabase database = mongoClient.getDatabase("fisheman");
+        MongoCollection<Document> collection = database.getCollection("users");
+
+        Document document = new Document("name", "Café Con Leche")
+                .append("contact", new Document("phone", "228-555-0149")
+                        .append("email", "cafeconleche@example.com")
+                        .append("location", Arrays.asList(-73.92502, 40.8279556)))
+                .append("stars", 3)
+                .append("categories", Arrays.asList("Bakery", "Coffee", "Pastries"));
+
+        collection.insertOne(document);
+        collection.find().forEach(printBlock);
+
+        System.out.println(database);
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 703, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 600, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Application().setVisible(true);
+            }
+        });
+    }
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+}
